c3afd399cba9a8554aa373b708eed21a08fc4cd3
corteshs <corteshs@ensiarchi13.imag.fr>
2013-09-25 16:37:09 +0200

    Merge

---
+++ src/not/CMakeFiles/allocphy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/allocphy.dir/src/mem.c.o"
+  "liballocphy.pdb"
+  "liballocphy.a"
+  "liballocphy.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/allocphy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/not/CMakeFiles/allocphy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for allocphy.
+# This may be replaced when dependencies are built.
